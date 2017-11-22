564e966fbf06fa6004a2b2a7f45c4a32974dd222
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-22 05:25:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var name = "Joe"
 var height = "74"
 
-
-// Don't worry about this
-module.exports = { name, height
+if (name==="Susan"){
+  console.log("")
 }
-
-```javascript
-if (name==="Susan")
